a5ce02339b8b1a89dc191fec03e158ca666eb97a
Casper Beyer <caspervonb@pm.me>
2020-07-11 07:25:43 +0800

    Add fseek(3) integration test
    This adds an integration test to ensure that fseek(3) is able to seek as
    expected on a file.

---
+++ integration/fseek.c
@@ -0,0 +1,29 @@
+// { "preopens": { "/scratch": "scratch" } }
+
+#include <assert.h>
+#include <stdio.h>
+
+int main(void) {
+	FILE *file = fopen("/scratch/output.txt", "w");
+	assert(file != NULL);
+
+	char data[32] = { 0 };
+	assert(fwrite(data, sizeof(char), sizeof(data), file) == 32);
+
+	assert(fseek (file, 8, SEEK_SET) == 0);
+	assert(ftell(file) == 8);
+
+	assert(fseek (file, 16, SEEK_SET) == 0);
+	assert(ftell(file) == 16);
+
+	assert(fseek (file, -16, SEEK_CUR) == 0);
+	assert(ftell(file) == 0);
+
+	assert(fseek (file, 16, SEEK_CUR) == 0);
+	assert(ftell(file) == 16);
+
+	assert(fseek (file, 0, SEEK_END) == 0);
+	assert(ftell(file) == 32);
+
+	assert(fclose(file) == 0);
+}
