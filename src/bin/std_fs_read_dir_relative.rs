812135186f1c6cb2594adf8adbccb8efc91c206d
Casper Beyer <caspervonb@pm.me>
2020-07-31 08:23:03 +0800

    Add relative variants of std::fs tests
    This adds a set of relative variants of the std::fs tests for more
    extensive coverage of pre-opened directories.

---
+++ src/bin/std_fs_read_dir_relative.rs
@@ -0,0 +1,13 @@
+// {
+//     "preopens": {
+//         "fixture": "fixture"
+//     }
+// }
+
+fn main() {
+  let entries = std::fs::read_dir("fixture").unwrap();
+  assert_eq!(entries.count(), 4);
+
+  let entries = std::fs::read_dir("fixture/directory").unwrap();
+  assert_eq!(entries.count(), 2);
+}
---
+++ src/bin/std_fs_write_relative.rs
@@ -0,0 +1,12 @@
+// {
+//     "preopens": {
+//         "scratch": "scratch"
+//     },
+//     "files": {
+//         "scratch/file": "file"
+//     }
+// }
+
+fn main() {
+  assert!(std::fs::write("scratch/file", b"file").is_ok())
+}
