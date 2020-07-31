96006ff1e9ca9ba01e4f15ad2d5d0b1ccb9d50e0
Casper Beyer <caspervonb@pm.me>
2020-07-31 08:23:03 +0800

    Add relative variants of std::fs tests
    This adds a set of relative variants of the std::fs tests for more
    extensive coverage of pre-opened directories.

---
+++ src/bin/std_fs_file_create_relative.rs
@@ -0,0 +1,10 @@
+// {
+//     "preopens": {
+//         "scratch": "scratch"
+//     }
+// }
+
+fn main() {
+  assert!(std::fs::File::create("scratch/file").is_ok());
+  assert!(std::fs::metadata("scratch/file").unwrap().is_file());
+}
