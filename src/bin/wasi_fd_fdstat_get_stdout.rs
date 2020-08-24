23d8504686f30467c934a323ab0870e1fc954d55
Casper Beyer <caspervonb@pm.me>
2020-08-25 02:40:07 +0800

    Add wasi::fd_fdstat_get tests
    This adds a couple of tests to ensure that wasi::fd_fdstat_get returns
    the correct type and flags for stdin, stdout and stderr.
    
    Rights to be determined at a later point and are not asserted.

---
+++ src/bin/wasi_fd_fdstat_get_stdout.rs
@@ -0,0 +1,10 @@
+// {
+// }
+
+fn main() {
+    unsafe {
+        let fdstat = wasi::fd_fdstat_get(wasi::FD_STDOUT).unwrap();
+        assert_eq!(fdstat.fs_filetype, wasi::FILETYPE_CHARACTER_DEVICE);
+        assert!((fdstat.fs_flags & wasi::FDFLAGS_APPEND) != 0);
+    }
+}
