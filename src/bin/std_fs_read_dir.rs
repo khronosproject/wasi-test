aba7e661b4ae66564156a707098368f284c3d688
Casper Beyer <caspervonb@pm.me>
2020-10-22 22:43:44 +0000

    Fix std::fs::read_dir test expectations
    The std::fs::read_dir test has the wrong expectations after a new
    fixture was added to the fixtures directory.
    
    This corrects that.

--- src/bin/std_fs_read_dir.rs
+++ src/bin/std_fs_read_dir.rs
@@ -6,7 +6,7 @@
 
 fn main() {
   let entries = std::fs::read_dir("fixtures").unwrap();
-  assert_eq!(entries.count(), 4);
+  assert_eq!(entries.count(), 5);
 
   let entries = std::fs::read_dir("fixtures/directory").unwrap();
   assert_eq!(entries.count(), 2);
