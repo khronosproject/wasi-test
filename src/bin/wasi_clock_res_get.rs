e4808a8ca21f61dce0cb339ad6721e564925ea5a
Dan Gohman <dev@sunfishcode.online>
2021-02-10 18:46:50 -0800

    Remove the process and thread clock tests
    Remove the tests for `CLOCKID_PROCESS_CPUTIME_ID` and
    `CLOCKID_THREAD_CPUTIME_ID`, since these values were
    [removed from the WASI spec].
    
    [removed from the WASI spec]: https://github.com/WebAssembly/WASI/pull/197

--- src/bin/wasi_clock_res_get.rs
+++ src/bin/wasi_clock_res_get.rs
@@ -11,21 +11,9 @@ unsafe fn test_clock_res_get_realtime() {
     assert!(res > 0);
 }
 
-unsafe fn test_clock_res_get_process_cputime_id() {
-    let res = wasi::clock_res_get(wasi::CLOCKID_PROCESS_CPUTIME_ID).unwrap();
-    assert!(res > 0);
-}
-
-unsafe fn test_clock_res_get_thread_cputime_id() {
-    let res = wasi::clock_res_get(wasi::CLOCKID_THREAD_CPUTIME_ID).unwrap();
-    assert!(res > 0);
-}
-
 fn main() {
     unsafe {
         test_clock_res_get_monotonic();
         test_clock_res_get_realtime();
-        test_clock_res_get_process_cputime_id();
-        test_clock_res_get_thread_cputime_id();
     }
 }
